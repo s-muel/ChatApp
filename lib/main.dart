c556b1de692b65f5e59e1f170515f335427ed255
s-muel <samuel.essuman@amalitech.org>
2021-12-01 12:49:09 +0000

    added a card

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: const ChatApp(),
     );
@@ -29,12 +29,43 @@ class ChatApp extends StatelessWidget {
         title: const Text("My Trial App"),
         actions: const [Icon(Icons.search), Icon(Icons.more)],
       ),
-      body: const Center(
-        child: CircleAvatar(
-          radius: 70,
-          backgroundColor: Colors.blue,
-          backgroundImage: NetworkImage(
-              "https://images.unsplash.com/photo-1637361890611-69854d19360e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"),
+      body: Column(
+        children: const [
+          ChatWidget()
+          
+        ],
+      ),
+    );
+  }
+}
+
+class ChatWidget extends StatelessWidget {
+  
+  const ChatWidget({
+    Key? key,
+  }) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      color: Colors.blue,
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: ListTile(
+          leading: const CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1637361890611-69854d19360e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"),
+            radius: 30,
+          ),
+          title: Text("Samueel"),
+          subtitle: Row(
+            children: const [
+              Icon(Icons.done_all),
+              Text("Hello"),
+            ],
+          ),
+          trailing: const Text("12:00"),
         ),
       ),
     );
